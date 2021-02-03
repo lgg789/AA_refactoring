a8c5188c4f3b9f3321a172df27ecead80839dd22
LAPTOP-2VF34Q37\lgg78 <lgg789@naver.com>
2021-02-03 17:56:00 +0900

    limit 설정누락부분 수정

--- VideoRental/CD.java
+++ VideoRental/CD.java
@@ -4,6 +4,7 @@ public class CD extends Video{
 
 	public CD(String title, int priceCode, Date registeredDate) {
 		super(title, priceCode, registeredDate);
+		setLimit(2));
 		// TODO Auto-generated constructor stub		
 	}
 	@Override
--- VideoRental/DVD.java
+++ VideoRental/DVD.java
@@ -1,9 +1,10 @@
 import java.util.Date;
 
-public class DVD extends Video {
+public class DVD extends Video{
 
-	public DVD(String title, int priceCode, Date registeredDate) {
-		super(title, priceCode, registeredDate);
+	public DVD(String title, int videoType, int priceCode, Date registeredDate) {
+		super(title, videoType, priceCode, registeredDate);
+		setLimit(3);
 		// TODO Auto-generated constructor stub
 	}
 

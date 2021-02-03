d6ff02797a6f23d5dd39ac24211f1bb6fb2887d9
LAPTOP-2VF34Q37\lgg78 <lgg789@naver.com>
2021-02-03 18:05:26 +0900

    build error 수정

--- VideoRental/DVD.java
+++ VideoRental/DVD.java
@@ -2,8 +2,8 @@ import java.util.Date;
 
 public class DVD extends Video{
 
-	public DVD(String title, int videoType, int priceCode, Date registeredDate) {
-		super(title, videoType, priceCode, registeredDate);
+	public DVD(String title, int priceCode, Date registeredDate) {
+		super(title, priceCode, registeredDate);
 		setLimit(3);
 		// TODO Auto-generated constructor stub
 	}
--- VideoRental/RentalItemFactory.java
+++ VideoRental/RentalItemFactory.java
@@ -5,18 +5,19 @@ public class RentalItemFactory {
 	public Video createVideo(String title, int videoType, int priceCode)
 	{
 		Video video = null;
-				
-		if (title.equals("DVD"))
+		
+		/*1 for VHD, 2 for CD, 3 for DVD*/
+		switch (videoType)
 		{
-			video = new DVD(title, videoType, new Date());	
-		}
-		else if (title.equals("CD"))
-		{
-			video = new CD(title, videoType, new Date());
-		}
-		else if (title.equals("VHS"))
-		{
-			video = new VHS(title, videoType, new Date());
+		case 1:
+			video = new VHS(title, priceCode, new Date());			
+			break;
+		case 2:
+			video = new CD(title, priceCode, new Date());
+			break;		
+		case 3:
+			video = new DVD(title, priceCode, new Date());
+			break;
 		}
 		
 		return video;
